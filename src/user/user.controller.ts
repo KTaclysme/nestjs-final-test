d79a21d706048f62359d3155d30752e93775ceed
KTaclysme <takevin.pro@gmail.com>
2024-04-11 23:04:59 +0200

    User config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { AddUserDto } from './dto/add.user.dto';
+import { UserService } from './user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
-    constructor() {}
+    constructor(private userService: UserService) {}
+
+    @Post()
+    addUser(@Body() addUserDto: AddUserDto) {
+        return this.userService.addUser(addUserDto);
+    }
+
+    @Get()
+    getUser(){
+        return this.userService.getUser();
+    }
+
+    @Delete()
+    resetData(){
+        return this.userService.resetData();
+    }
 }
