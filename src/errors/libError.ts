12b8a047d74b8298331ad596cf2ed1a1e9d5537d
kevin.ta <kevin.ta@emeria.eu>
2024-05-18 10:46:25 +0200

    errorlib

--- src/errors/libError.ts
+++ src/errors/libError.ts
@@ -1,1 +1,9 @@
-export class ErrorDetails {}
+import { HttpExceptionBodyMessage, HttpStatus } from '@nestjs/common';
+
+export class ErrorDetails {
+    description: string;
+    errorType: string;
+    message: HttpExceptionBodyMessage;
+    error: HttpStatus;
+    statusCode: number;
+}
